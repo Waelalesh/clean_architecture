f0717d2393d3d028a31656b71bdb14bffc42e3a4
waelalash04 <wael.alash04@gmail.com>
2025-04-30 13:52:03 +0300

    add dartz package to use either class add failure class to fech failure case add generic use case add domain layer in the structrue add home repo & book entity & fech featured books , fech newest books use cases done the domain layer

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,8 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String autherName;
+  final num price;
+  final num rating;
+  BookEntity(this.autherName, this.image, this.price, this.rating, this.title);
+}
---
+++ lib/Features/home/domain/use_cases/fech_newest_books_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FechNewestBooksUseCase {
+  HomeRepo homeRepo;
+
+  FechNewestBooksUseCase(this.homeRepo);
+  Future<Either<Failure, List<BookEntity>>> call([NoParams? params]) async {
+    return await homeRepo.fetchNewsetBooks();
+  }
+}
